b209ec77db5c179e253326dc050b1c1808432248
Kollan House <kollanhouse@Kollans-MacBook-Pro.local>
2025-07-20 13:18:58 -0700

    adds additional scripts and updates omnipair metadata

--- scripts/initializeLaunch.ts
+++ scripts/initializeLaunch.ts
@@ -41,6 +41,12 @@ const ONE_DAY_IN_SECONDS = ONE_HOUR_IN_SECONDS * 24;
 const SEVEN_DAYS_IN_SECONDS = ONE_DAY_IN_SECONDS * 7;
 
 async function main() {
+  
+  console.log('Payer address:', payer.publicKey.toBase58());
+  console.log('Authorized launcher key address:', START_LAUNCH_KEY.toBase58());
+  console.log('Spending limit: $', SPENDING_LIMIT);
+  console.log('Minimum raise amount: $', MIN_RAISE_AMOUNT);
+
   const seed = TOKEN_SEED;
   const TOKEN = await PublicKey.createWithSeed(
     payer.publicKey,
